a616ba2f005b695428b6ed08975290152831a47b
RyanHoo <hbkeong95@gmail.com>
2019-10-30 22:24:58 +0800

    reset config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    URL: process.env.BASE_URL,
-    MONGODB_URI: 'mongodb+srv://abc1234:abc1234@cluster0-t8dzl.mongodb.net/test?retryWrites=true&w=majority'
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://abc1234:abc1234@cluster0-t8dzl.mongodb.net/test?retryWrites=true&w=majority'
 }
