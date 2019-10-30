22143e0c06dc6f2fb54e4b73dd1ffc2dcaf8de1f
RyanHoo <hbkeong95@gmail.com>
2019-10-30 22:13:31 +0800

    make it better

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
-    URL: process.env.BASE_URL || 'https://guarded-savannah-22827.herokuapp.com',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://abc1234:abc1234@cluster0-t8dzl.mongodb.net/test?retryWrites=true&w=majority'
+    URL: process.env.BASE_URL,
+    MONGODB_URI: 'mongodb+srv://abc1234:abc1234@cluster0-t8dzl.mongodb.net/test?retryWrites=true&w=majority'
 }
